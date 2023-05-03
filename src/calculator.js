c605119c891f21b1443a4e56e7f070e1d9987717
이동훈 <94donghoon.lee@gmail.com>
2023-05-03 12:13:38 +0900

    add: calculator.js

---
+++ src/calculator.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View } from 'react-native';
+
+
+// Button type: 'reset' | 'operator' | 'num'
+const Button = ({ text, onPress, flex, type }) => {
+  return (
+    <TouchableOpacity style={{}}>
+
+    </TouchableOpacity>
+  )
+}
+
+const COLOR = {
+  RESULT: '#4e4c51',
+  RESET: '#5f5e62',
+  OPERATOR: '#f39c29',
+  NUM: '#5c5674',
+}
+
+export default () => {
+  return (
+    <View>
+    </View>
+  )
+}
